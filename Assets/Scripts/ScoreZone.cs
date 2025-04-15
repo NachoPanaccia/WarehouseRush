6e5c598cb7a9565c540f92816f1bcbbe55748e4a
cosmear <92765555+cosmear@users.noreply.github.com>
2025-04-14 22:32:12 -0300

    pila terminada

--- Assets/Scripts/ScoreZone.cs
+++ Assets/Scripts/ScoreZone.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using System.Collections;
 
 public class ScoreZone : MonoBehaviour
 {
-    public string validBoxTag = "Box"; // <- etiqueta de la caja correcta
+    public string validBoxTag = "Box";
+    private int cajasRecibidas = 0;
+    [SerializeField] private float delayAntesDeDestruir = 2f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(validBoxTag))
         {
-            ScoreManager.Instance.AddPoint();
-            Debug.Log("¡Caja entregada!");
+            cajasRecibidas++;
+
+            // Hacemos hijo el objeto dentro del camión
+            other.transform.SetParent(transform);
+
+            if (cajasRecibidas >= 2)
+            {
+                StartCoroutine(DestruirConDelay());
+            }
         }
+    }
+
+    private IEnumerator DestruirConDelay()
+    {
+        Debug.Log("Camión completo. Destruyendo en " + delayAntesDeDestruir + " segundos...");
+        yield return new WaitForSeconds(delayAntesDeDestruir);
+        Destroy(gameObject);
     }
 }
 
