a3401e3aaccbb7bb267d1e7b1125b3528d1a1966
Ignacio Panaccia <Panaccia01@gmail.com>
2025-04-27 20:58:56 -0300

    Terminados Scripts de desplazamiento de Cinta Transportadora

--- Assets/Scripts/Cinta Transportadora/CintaTransportadora.cs
+++ Assets/Scripts/Cinta Transportadora/CintaTransportadora.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 
 public class CintaTransportadora : MonoBehaviour
 {
-    private int movementSpeed;
-    private LinkedList<GameObject> cajasADesplazar;
+    [SerializeField] private LinkedList<GameObject> cajasADesplazar = new LinkedList<GameObject>();
 
-    [Header("Puntos de movimiento")]
+    [Header("Movimiento de las cajas")]
     [SerializeField] private Transform puntoInicial;
     [SerializeField] private Transform puntoFinal;
 
-    private void Start()
+    [SerializeField] private float movementSpeed = 1f;
+
+    private void Update()
     {
-        cajasADesplazar = new LinkedList<GameObject>();
+        DesplazarCajas();
+    }
+
+    public void AgregarCaja(GameObject cajaPrefab)
+    {
+        GameObject nuevaCaja = Instantiate(cajaPrefab, puntoInicial.position, Quaternion.identity);
+        cajasADesplazar.AddLast(nuevaCaja);
+        Debug.Log("Se agregó una caja a la cinta.");
+    }
+
+    private void DesplazarCajas()
+    {
+        if (cajasADesplazar.Count == 0)
+            return;
+
+        var nodoActual = cajasADesplazar.First;
+
+        while (nodoActual != null)
+        {
+            GameObject caja = nodoActual.Value;
+            if (caja != null)
+            {
+                caja.transform.position = Vector3.MoveTowards(caja.transform.position, puntoFinal.position, movementSpeed * Time.deltaTime);
+
+                if (Vector3.Distance(caja.transform.position, puntoFinal.position) < 0.1f)
+                {
+                    var siguienteNodo = nodoActual.Next;
+                    cajasADesplazar.Remove(nodoActual);
+                    nodoActual = siguienteNodo;
+                    continue;
+                }
+            }
+
+            nodoActual = nodoActual.Next;
+        }
     }
 }
