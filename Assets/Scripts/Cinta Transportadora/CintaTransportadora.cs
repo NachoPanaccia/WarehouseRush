5eda93bbad1b98eb3842732de2b473493e083925
Ignacio Panaccia <Panaccia01@gmail.com>
2025-04-26 23:28:57 -0300

    Script de Cola de Cajas listo

---
+++ Assets/Scripts/Cinta Transportadora/CintaTransportadora.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class CintaTransportadora : MonoBehaviour
+{
+    [SerializeField] private Transform boxSpawnPosition;
+    [SerializeField] private int spawnSpeed;
+    [SerializeField] private int cantidadDeCajas;
+    private int[] arrayCajas;
+
+    private float nextSpawnTime = 0f;
+    private int indexActual;
+
+    [Header("Posibles Cajas")]
+    [SerializeField] private GameObject caja1;
+    [SerializeField] private GameObject caja2;
+
+
+    void Start()
+    {
+        ColaDeCajas();
+    }
+    
+    void Update()
+    {
+        if (indexActual >= arrayCajas.Length)
+            return;
+
+        if (Time.time >= nextSpawnTime)
+        {
+            SpawnCaja(arrayCajas[indexActual]);
+            nextSpawnTime = Time.time + spawnSpeed;
+            indexActual++;
+        }
+    }
+
+    void ColaDeCajas()
+    {
+        indexActual = 0;
+        arrayCajas = new int[cantidadDeCajas];
+
+        for (int i = 0; i < cantidadDeCajas; i++)
+        {
+            arrayCajas[i] = Random.Range(1, 3);
+        }
+    }
+
+    void SpawnCaja(int tipoCaja)
+    {
+        GameObject cajaPrefab = null;
+
+        if (tipoCaja == 1)
+        {
+            cajaPrefab = caja1;
+        }
+        else if (tipoCaja == 2)
+        {
+            cajaPrefab = caja2;
+        }
+
+        if (cajaPrefab != null)
+            Instantiate(cajaPrefab, boxSpawnPosition.position, Quaternion.identity);
+    }
+}
