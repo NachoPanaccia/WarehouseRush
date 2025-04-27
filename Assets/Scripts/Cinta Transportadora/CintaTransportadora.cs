b8c331803213f3e7e0fd136e48da7ed3fb2a04e5
Ignacio Panaccia <Panaccia01@gmail.com>
2025-04-27 14:57:24 -0300

    Ultimo merge

--- Assets/Scripts/Cinta Transportadora/CintaTransportadora.cs
+++ Assets/Scripts/Cinta Transportadora/CintaTransportadora.cs
@@ -1,63 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CintaTransportadora : MonoBehaviour
 {
-    [SerializeField] private Transform boxSpawnPosition;
-    [SerializeField] private int spawnSpeed;
-    [SerializeField] private int cantidadDeCajas;
-    private int[] arrayCajas;
+    private int movementSpeed;
+    private LinkedList<GameObject> cajasADesplazar;
 
-    private float nextSpawnTime = 0f;
-    private int indexActual;
+    [Header("Puntos de movimiento")]
+    [SerializeField] private Transform puntoInicial;
+    [SerializeField] private Transform puntoFinal;
 
-    [Header("Posibles Cajas")]
-    [SerializeField] private GameObject caja1;
-    [SerializeField] private GameObject caja2;
-
-
-    void Start()
+    private void Start()
     {
-        ColaDeCajas();
-    }
-    
-    void Update()
-    {
-        if (indexActual >= arrayCajas.Length)
-            return;
-
-        if (Time.time >= nextSpawnTime)
-        {
-            SpawnCaja(arrayCajas[indexActual]);
-            nextSpawnTime = Time.time + spawnSpeed;
-            indexActual++;
-        }
-    }
-
-    void ColaDeCajas()
-    {
-        indexActual = 0;
-        arrayCajas = new int[cantidadDeCajas];
-
-        for (int i = 0; i < cantidadDeCajas; i++)
-        {
-            arrayCajas[i] = Random.Range(1, 3);
-        }
-    }
-
-    void SpawnCaja(int tipoCaja)
-    {
-        GameObject cajaPrefab = null;
-
-        if (tipoCaja == 1)
-        {
-            cajaPrefab = caja1;
-        }
-        else if (tipoCaja == 2)
-        {
-            cajaPrefab = caja2;
-        }
-
-        if (cajaPrefab != null)
-            Instantiate(cajaPrefab, boxSpawnPosition.position, Quaternion.identity);
+        cajasADesplazar = new LinkedList<GameObject>();
     }
 }
